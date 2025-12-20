4b566b732f109813bd56e650fc24af6698384919
kentangkotak <ariezlnd69@gmail.com>
2025-12-20 10:55:54 +0700

    wew

--- src/stores/master/pekerjaan.js
+++ src/stores/master/pekerjaan.js
@@ -1,13 +1,16 @@
 import { defineStore } from 'pinia'
+import { api } from 'src/boot/axios'
+import { notifSuccessVue } from 'src/modules/utils'
 
 export const usePekerjaanStore = defineStore('pekerjaan', {
   state: () => ({
-    loading: false,
+    loadingSave: false,
+    items: [],
     params: {
       q: null
     },
     form: {
-      pekerjaan: null
+      pekerjaan: ''
     }
   }),
   actions: {
@@ -16,6 +19,29 @@ export const usePekerjaanStore = defineStore('pekerjaan', {
     },
     refresh () {
       this.params.q = null
+    },
+    async simpanData() {
+      this.loadingSave = true
+      try {
+        const resp = await api.post('v1/master/pekerjaan/save', this.form)
+
+        if (resp.success === true) {
+          this.items.unshift(resp?.data?.data)
+          notifSuccessVue(resp?.data?.message)
+          this.loadingSave = false
+          this.initForm()
+
+        }
+      } catch (error) {
+        console.log(error)
+        this.loadingSave = false
+        notifErrVue(error?.response?.data?.message)
+      }
+    },
+    initForm() {
+      this.form = {
+        pekerjaan: ''
+      }
     }
   }
 })
