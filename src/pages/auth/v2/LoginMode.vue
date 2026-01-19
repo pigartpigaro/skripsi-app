00357d50f23f14a6f9412a33f8f85ef2cfab3092
duro06 <andi.meka.025@gmail.com>
2026-01-19 14:51:45 +0700

    rev login mobile

--- src/pages/auth/v2/LoginMode.vue
+++ src/pages/auth/v2/LoginMode.vue
@@ -29,7 +29,7 @@
             class="login-form col-md-6 col-lg-6 col-xl-6 col-sm-6 col-xs-12 min-h">
             <FormLogin :key="mode" />
           </div>
-          <div v-else-if="mode === 'register' && isMobile"
+          <div v-else-if="mode === 'form-login' && isMobile"
             class="login-form col-md-6 col-lg-6 col-xl-6 col-sm-6 col-xs-12 min-h">
             <FormLogin :key="isMobile" />
           </div>
